2002b56f48beb1256cc57b3287cff3a7c4b02d41
TogrulZade <TogrulZade@gmail.com>
2021-05-11 08:51:14 +0400

    user migrate

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,14 +14,14 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            // $table->bigIncrements('id');
-            // $table->string('name');
+            $table->bigIncrements('id');
+            $table->string('name');
             $table->string('editable');
-            // $table->string('email')->unique();
-            // $table->timestamp('email_verified_at')->nullable();
-            // $table->string('password');
-            // $table->rememberToken();
-            // $table->timestamps();
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
         });
     }
 
