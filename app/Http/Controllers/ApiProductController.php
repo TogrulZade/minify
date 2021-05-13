cb304f7dc8ba4584854c5f43b6895745a627b45d
TogrulZade <TogrulZade@gmail.com>
2021-05-13 15:03:42 +0400

    api

--- app/Http/Controllers/ApiProductController.php
+++ app/Http/Controllers/ApiProductController.php
@@ -12,7 +12,7 @@ class ApiProductController extends Controller
     {
         $pid = $request->pid;
         // $data = Product::with('pictures')->with('city')->with('market')->orderBy('created_at',"DESC")->where('id',"=",$pid)->first();
-        $data = DB::table("products")->join("pictures","products.id","=","product_id")->leftJoin('markets','markets.id','products.market_id')->where("products.id", "=", $pid)->leftJoin('cities','cities.id','products.city_id')->select("products.*","markets.*","markets.name as market","pictures.*","cities.name as city")->first();
+        $data = DB::table("products")->join("pictures","products.id","=","product_id")->leftJoin('markets','markets.id','products.market_id')->where("products.id", "=", $pid)->leftJoin('cities','cities.id','products.city_id')->select("products.*","markets.*","markets.name as market","pictures.*","cities.name as city")->get();
         return $data;
     }
 }
