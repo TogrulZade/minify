966250eac9d03b9df80f33b5362913348e298d7e
TogrulZade <TogrulZade@gmail.com>
2021-05-29 02:23:35 +0400

    migration

---
+++ database/migrations/2021_05_28_222218_add_finished_at_to_products.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddFinishedAtToProducts extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->timestamp('started_at')->default(\DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('closed_at')->default(\DB::raw('CURRENT_TIMESTAMP'));
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('products', function (Blueprint $table) {
+            //
+        });
+    }
+}
