cb2f2534a6bcdba4c6ab186578b9c82922f95ff6
TogrulZade <TogrulZade@gmail.com>
2022-03-13 14:26:39 +0400

    .

--- resources/views/Admin/Admin.blade.php
+++ resources/views/Admin/Admin.blade.php
@@ -25,6 +25,7 @@
 
                     <div class="col-xs-12 mb-1">
                         <div onclick="window.location.href='/admin/delete/{{$pr->id}}'; return false" class="btn btn-danger btn-block delete-link">Remove</div>
+                        
                     </div>
                 <div class="card-title-wrap">
                     <div class="card-title">
