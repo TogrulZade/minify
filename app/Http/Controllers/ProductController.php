c2b18e6fe3bd20571691e4f82fb39fb2ec2980d5
TogrulZade <TogrulZade@gmail.com>
2022-07-21 15:03:19 +0400

    /

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -300,7 +300,11 @@ class ProductController extends Controller
 				$subject = $request->product_name." - Minify.az-a əlavə edildi<br/>";
 				$txt = "Title: ".$request->product_name."<br/>Description: ".$request->product_description."<br/> Market:".$request->merchant_number."<br/> User: ".Auth::user()->id."<br/>Price: ".$request->product_price."<br/>";
 				$headers = "From: togrulzade@gmail.com" . "\r\n";
-				mail($to,$subject,$txt,$headers);
+				if(mail($to,$subject,$txt,$headers)){
+					echo "Mail sent";
+				}else{
+					echo "error";
+				}
 			// error_reporting($errLevel);  // restore old error levels
 			
 			
