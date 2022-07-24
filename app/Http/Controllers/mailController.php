7908cda6ffbdc8d35b7fdae7fbd6c7efdf1a2735
TogrulZade <TogrulZade@gmail.com>
2022-07-24 22:56:08 +0400

    .

--- app/Http/Controllers/mailController.php
+++ app/Http/Controllers/mailController.php
@@ -8,13 +8,18 @@ class mailController extends Controller
 {
     public function send()
     {   
-        $data = [];
-        Mail::send('mail.productCreated', $data, function ($message) {
-            $message->from('togrulzade@gmail.com', 'Minify Sistem');
-            $message->sender('togrulzade@gmail.com', 'John Doe');
-            $message->to('togrul.zade@yandex.ru', 'John Doe');
-            $message->subject('Subject');
-            $message->priority(3);
-        });
+        $data = ["name"=>"Togrul Zade"];
+        // Mail::send('mail.productCreated', $data, function ($message) {
+        //     $message->from('adminminify@minify.az', 'Minify Sistem');
+        //     $message->sender('adminminify@minify.az', 'John Doe');
+        //     $message->to('adminminify@minify.az', 'John Doe');
+        //     $message->subject('Subject');
+        //     $message->priority(3);
+        // });
+        Mail::send(['text'=>"mail.productCreated"], $data, function($message){
+            $message->to('adminminify@minify.az', 'Tutorials Point')->subject
+            ('Laravel Basic Testing Mail');
+            $message->from('adminminify@minify.az','Virat Gandhi');
+        } );
     }
 }
